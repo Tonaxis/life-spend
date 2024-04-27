aff9cdd0bc9610b7490ea782dc97e75d2ff6f349
Tonaxis <83670267+Tonaxis@users.noreply.github.com>
2024-04-27 20:39:58 +0200

    docker

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 // type Sexe = 'MEN' | 'WOMEN';
 function App() {
-  const [birthday, setBirthday] = useState(new Date('2023-10-14'));
-  // const [birthday, setBirthday] = useState(new Date('2003-06-04'));
-  const [sexe, setSexe] = useState('MEN');
+  const [birthday, setBirthday] = useState(new Date("2023-10-14"));
+  const [sexe, setSexe] = useState("MEN");
   const [age, setAge] = useState(0);
 
-  const [maxAge, setMaxAge] = useState(79.3);
+  const [maxAge, setMaxAge] = useState(79.4);
   const [maxDate, setMaxDate] = useState(new Date());
 
   const [percentOfCurrentYear, setPercentOfCurrentYear] = useState(0);
@@ -17,8 +16,8 @@ function App() {
 
   useEffect(() => {
     setAge(new Date(new Date() - birthday).getFullYear() - 1970);
-    if (sexe === 'MEN') setMaxAge(79.3);
-    else setMaxAge(89.5);
+    if (sexe === "MEN") setMaxAge(79.4);
+    else setMaxAge(85.3);
 
     calculCurrentDay();
     clearInterval(intervalRef.current);
@@ -27,33 +26,52 @@ function App() {
 
       calculPercentOfCurrentDay();
     }, 1000);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [birthday, sexe]);
 
   useEffect(() => {
-    setMaxDate(new Date(Math.round(birthday.getTime() + (maxAge * (1000 * 60 * 60 * 24 * 365.25)))));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [maxAge])
+    setMaxDate(
+      new Date(
+        Math.round(birthday.getTime() + maxAge * (1000 * 60 * 60 * 24 * 365.25))
+      )
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [maxAge]);
 
   function calculCurrentDay() {
     let today = new Date();
     let year = today.getFullYear();
-    if (new Date().getTime() < new Date(year, birthday.getMonth(), birthday.getDate()).getTime()) year--;
-    let lastDayOfYear = new Date(year + 1, birthday.getMonth(), birthday.getDate()).getTime();
-    let firstDayOfYear = new Date(year, birthday.getMonth(), birthday.getDate()).getTime();
-    setPercentOfCurrentYear(((today.getTime() - firstDayOfYear) * 100) / (lastDayOfYear - firstDayOfYear));
+    if (
+      new Date().getTime() <
+      new Date(year, birthday.getMonth(), birthday.getDate()).getTime()
+    )
+      year--;
+    let lastDayOfYear = new Date(
+      year + 1,
+      birthday.getMonth(),
+      birthday.getDate()
+    ).getTime();
+    let firstDayOfYear = new Date(
+      year,
+      birthday.getMonth(),
+      birthday.getDate()
+    ).getTime();
+    setPercentOfCurrentYear(
+      ((today.getTime() - firstDayOfYear) * 100) /
+        (lastDayOfYear - firstDayOfYear)
+    );
   }
 
   function lifeUsed() {
-    let today = (new Date()).getTime() - birthday.getTime();
+    let today = new Date().getTime() - birthday.getTime();
     let max = maxDate.getTime() - birthday.getTime();
-    return ((today * 100) / max).toFixed(2)
+    return ((today * 100) / max).toFixed(2);
   }
 
   function getYearCaseStyle(year, current, past, future) {
-    if (age - (year) === 0) {
+    if (age - year === 0) {
       return current;
-    } else if (age - (year) > 0) {
+    } else if (age - year > 0) {
       return past;
     } else {
       return future;
@@ -62,12 +80,13 @@ function App() {
 
   function calculPercentOfCurrentDay() {
     let today = new Date();
-    let seconds = today.getSeconds() + (60 * (today.getMinutes() + (60 * today.getHours())));
+    let seconds =
+      today.getSeconds() + 60 * (today.getMinutes() + 60 * today.getHours());
     return seconds / (60 * 60 * 24);
   }
 
   return (
-    <div className='App'>
+    <div className="App">
       <header>
         <img src="./logo.svg" alt="logo" />
         <h1>LIFE SPEND</h1>
@@ -75,36 +94,66 @@ function App() {
       <main>
         <div className="inputs">
           <div>
-            <label htmlFor='birthday'>Set your birthday </label>
-            <input id='birthday' type="date" onChange={(e) => setBirthday(new Date(e.target.value))} />
+            <label htmlFor="birthday">
+              Définissez votre date d'anniversaire{" "}
+            </label>
+            <input
+              id="birthday"
+              type="date"
+              onChange={(e) => setBirthday(new Date(e.target.value))}
+            />
           </div>
-          <div className='sexes'>
+          <div className="sexes">
             <label htmlFor="sexe">
               Sexe
-              <div className='switch-sexe'>
-                <div className='sexe men'>♂</div>
-                <div className='sexe women'>♀</div>
+              <div className="switch-sexe">
+                <div className="sexe men">♂</div>
+                <div className="sexe women">♀</div>
               </div>
             </label>
-            <input id='sexe' type="checkbox" onChange={(e) => setSexe(e.target.checked ? 'MEN' : 'WOMEN')} checked={sexe === 'MEN'} />
+            <input
+              id="sexe"
+              type="checkbox"
+              onChange={(e) => setSexe(e.target.checked ? "MEN" : "WOMEN")}
+              checked={sexe === "MEN"}
+            />
           </div>
         </div>
-        <div className='years'>
-          {
-            new Array(Math.ceil(maxAge > age ? maxAge : age)).fill(0).map((_, i) => (
-              <div key={i + 1} className='year' style={{
-                '--gone': getYearCaseStyle(i, `${percentOfCurrentYear}%`, `100%`, `0%`),
-                '--color': (maxAge > i ? 'aquamarine' : 'gold'),
-                boxShadow: getYearCaseStyle(i, `0 0 0 1px white`, ``, ``)
-              }}></div>
-            ))
-          }
+        <div className="years">
+          {new Array(Math.ceil(maxAge > age ? maxAge : age))
+            .fill(0)
+            .map((_, i) => (
+              <div
+                key={i + 1}
+                className="year"
+                style={{
+                  "--gone": getYearCaseStyle(
+                    i,
+                    `${percentOfCurrentYear}%`,
+                    `100%`,
+                    `0%`
+                  ),
+                  "--color": maxAge > i ? "aquamarine" : "gold",
+                  boxShadow: getYearCaseStyle(i, `0 0 0 1px white`, ``, ``),
+                }}
+              ></div>
+            ))}
         </div>
-        <p>You have used up <span className='percent-life'>{lifeUsed()}%</span> of your lifetime</p>
+        <p>
+          Vous avez utilisé <span className="percent-life">{lifeUsed()}%</span>{" "}
+          de votre temps de vie
+        </p>
       </main>
       <footer>
-        <p className='data'>Calculations based on the average life expectancy of a person in France. <span>79.3 years</span> for a man and <span>89.5 years</span> for a woman.</p>
-        <p className='warning'>These estimates are based on <strong>averages</strong> and are therefore not exact.</p>
+        <p className="data">
+          Calculs basés sur l'espérance de vie moyenne d'une personne en France.{" "}
+          <span>79.4ans</span> pour un homme et <span>85.3ans</span> pour une
+          femme.
+        </p>
+        <p className="warning">
+          Ces estimations sont basées sur des <strong>moyennes</strong> et ne
+          sont donc <strong>pas exactes</strong>.
+        </p>
       </footer>
     </div>
   );
